e986647d346c55a1369c465e2dc573c238dad113
jordidbmf <jordi@zamplex.com.br>
2023-08-29 16:10:15 -0300

    Finalizando a aula

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -3,9 +3,12 @@ public class CaixaEletronico {
         double saldo = 25.0;
         double valorSolicitado = 26.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }else
+            System.out.println("Saldo insuficiente");
 
-        System.out.println(saldo);
+        
     }
 }
